e82ac0905aad44d3ab05dbcb6eb4e6146697ff16
João Vale <carlosvalejcov@gmail.com>
2024-10-15 22:12:39 +0100

    outliers exploration

--- README.md
+++ README.md
@@ -1,37 +1,59 @@
 # DAA
 DAA group pratical work
 
-# Data Description
-## Transition
+## Table of Contents
+
+1. [Data Description](#data-description)
+2. [Data Exploration](#data-exploration)
+3. [Data Preprocessing](#data-preprocessing)
+4. [Undefined](#undefined)
+5. [Useful Links / Refs](#useful-links--refs)
+
+## Data Description
+### Transition
 - **CN**: Cognitive Normal, estado normal
 - **MCI**: Mild Cognitive Impairment, estado entre o avanço normal da perda de memoria com a idade e um certo declinio serio de demencia
 - **AD**: Azlheimer Disease, forma mais comum de demencia nas pessoas mais velhas
 
-# Data Exploration
-## Shape 
+
+## Data Exploration
+### Shape 
 - **Rows**: 305
 - **Columns**: 2181
 
-## Columns
-### Age
+### Columns
+#### Age
 - **Max**: 91
 - **Min**: 55.3
 - **Mean**: 75.1
 
-### Sex
+#### Sex
 - **Man**: 173 (57%)
 - **Woman**: 132 (43%)
+
 Relatively balanced
 
-### Transition
+#### Transition
 - **CN-CN**: 96
 - **MCI-MCI**: 71
 - **MCI-AD**: 68
 - **AD-AD**: 60
 - **CN-MCI**: 10
 
+#### Diagnostics Versions
+- **PyRadiomics**: 2.2.0
+- **Numpy**: 1.18.5
+- **SimpleITK**: 1.2.4
+- **PyWavelet**: 1.1.1
+- **Python**: 3.7.7
 
-# Useful links:
+
+## Data Preprocessing
+
+## Undefined
+
+
+## Useful Links / Refs
 1. **Alzheimer**: [Data](http://adni.loni.usc.edu/)
 2. **PyRadiomics**: [Documentation](https://pyradiomics.readthedocs.io/)
 3. **IQR ouliers method**: [IQR](https://builtin.com/articles/1-5-iqr-rule)
--- src/DataExploration.py
+++ src/DataExploration.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
 
 main_df = pd.read_csv("../Dataset/train_radiomics_occipital_CONTROL.csv")
 
@@ -8,16 +9,23 @@ main_df = pd.read_csv("../Dataset/train_radiomics_occipital_CONTROL.csv")
 # Matplotlib Plots #
 
 def show_histogram(title="histogram",df=main_df):
+    plt.figure(figsize=(13,8))
+    plt.subplots_adjust(bottom=0.17)
     plt.title(title)
     sns.histplot(df)
     plt.show()
 
 def show_boxplot(title="boxplot",columns=main_df.columns,df=main_df):
+    plt.figure(figsize=(13,8))
+    plt.subplots_adjust(bottom=0.17)
     df[columns].boxplot()
+    plt.xticks(rotation=15)
     plt.title(title)
     plt.show()
 
 def show_heatmap(title="correlation heatmap",df=main_df.select_dtypes(include="number")):
+    plt.figure(figsize=(13,8))
+    plt.subplots_adjust(bottom=0.17)
     plt.title(title)
     sns.heatmap(df.corr(),annot=True,cmap="coolwarm",linewidths=0.5)
     plt.show()
@@ -37,8 +45,22 @@ def detect_outliers(column, df=main_df):
 
     # Filtrar os outliers
     outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
+
+    show_boxplot(title=f"{column} boxplot",df=df,columns=[column])
+
     return outliers[column]
 
+# esta função faz um loop de 7 em 7 colunas por todas as colunas do dataset para uma analise geral dos outliers.
+# esta operacao é demorada e nao muito boa porque 2181 / 7 = 300 vezes
+def explore_outliers(df=main_df):
+    number_df = df.select_dtypes(include="number")
+    n_columns = number_df.columns
+    # normalização para ser visualmente perceptivel nos plots
+    scaler = MinMaxScaler()
+    number_df_scaled = pd.DataFrame(scaler.fit_transform(number_df),columns=n_columns)
+
+    for i in range(0, len(n_columns),7):
+        show_boxplot(df=number_df_scaled,columns=n_columns[i:i+7])
 
 #####################
 # Basic Exploration #
@@ -54,7 +76,6 @@ def categorical_exploration(df=main_df):
     MCI - Mild Cognitive Impairment, estado entre o avanço normal da perda de memoria com a idade e um certo declinio serio de demencia
     AD - Azlheimer Disease, forma mais comum de demencia nas pessoas mais velhas
     """
-    
     categorical_df = df[["Sex","Transition"]]
 
     for column in categorical_df.columns:
@@ -66,14 +87,24 @@ def numerical_exploration(df=main_df):
     age_exploration = df["Age"].describe()
     print(age_exploration)
     show_histogram("Age Histogram",df["Age"])
-    show_boxplot("Outliers Boxplot",["Age"],df)
     print(detect_outliers("Age"))
 
+    # show_heatmap()
 
 
-    show_heatmap(df=df[['Age', 'lbp-3D-k_ngtdm_Busyness', 'lbp-3D-k_ngtdm_Coarseness']])
+def diagnostics_versions_explorer(df=main_df):
+    diagnostics_versions_columns = ["diagnostics_Versions_PyRadiomics","diagnostics_Versions_Numpy","diagnostics_Versions_SimpleITK","diagnostics_Versions_PyWavelet","diagnostics_Versions_Python"] 
+
+    diagnostics_df = df[diagnostics_versions_columns]
+
+    for column in diagnostics_df.columns:
+        print(column,": ")
+        values = diagnostics_df[column].unique()
+        print(values)
 
 
-#main_info()
+
+# main_info()
 # categorical_exploration()
-numerical_exploration()
+# numerical_exploration()
+# explore_outliers()
